b9910bcfe8bc108557c46d2fcbff84d52ef5b639
Jose Lopez <joselopez11394@gmail.com>
2020-08-20 21:26:42 -0400

    added test to check if prompt is returned correctly

--- test/start_game_test.rb
+++ test/start_game_test.rb
@@ -8,4 +8,13 @@ class StartGameTest < Minitest::Test
 
     assert_instance_of StartGame, start_game
   end
+
+  def test_should_return_prompt
+    start_game = StartGame.new
+
+    expected = "Welcome to War! (or Peace) This game will be played
+    with 52 cards. The players today are Megan and Aurora."
+
+    assert_equal expected, start_game.prompt
+  end
 end
