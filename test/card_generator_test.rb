9bff0dd4b500c0d21be84d336d6319ed99514395
Jose Lopez <joselopez11394@gmail.com>
2020-08-22 14:17:05 -0400

    adds card generator class

---
+++ test/card_generator_test.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/card_generator"
+
+
+class CardGeneratorTest < Minitest::Test
+
+  def test_should_exists
+    filename = "cards.txt"
+    cards = CardGenerator.new(filename)
+
+    assert_instance_of CardGenerator, cards
+  end
+end
