0effed2b8a5473a0d991afadb404dfa592b17317
Jose Lopez <joselopez11394@gmail.com>
2020-08-22 14:21:06 -0400

    adds more test to card generator

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -11,4 +11,11 @@ class CardGeneratorTest < Minitest::Test
 
     assert_instance_of CardGenerator, cards
   end
+
+  def test_should_return_an_array_of_cards
+    filename = "cards.txt"
+    cards = CardGenerator.new(filename)
+
+    assert_equal "cards.txt", cards.filename
+  end
 end
