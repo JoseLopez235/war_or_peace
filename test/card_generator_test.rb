67cdefc1fafd157978be78e00b3e5e8764ff0313
Jose Lopez <joselopez11394@gmail.com>
2020-08-22 15:35:40 -0400

    finishes card generator and changes cards txt file

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -6,16 +6,23 @@ require "./lib/card_generator"
 class CardGeneratorTest < Minitest::Test
 
   def test_should_exists
-    filename = "cards.txt"
+    filename = "lib/cards.txt"
     cards = CardGenerator.new(filename)
 
     assert_instance_of CardGenerator, cards
   end
 
-  def test_should_return_an_array_of_cards
-    filename = "cards.txt"
+  def test_should_file_name
+    filename = "lib/cards.txt"
     cards = CardGenerator.new(filename)
 
-    assert_equal "cards.txt", cards.filename
+    assert_equal "lib/cards.txt", cards.filename
+  end
+
+  def test_should_return_an_array_of_cards
+    filename = "lib/cards.txt"
+    deck = CardGenerator.new(filename)
+
+    assert_equal 52, deck.cards.length
   end
 end
